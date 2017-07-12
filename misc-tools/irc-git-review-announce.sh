f881a59c61fe38a601a09b099c14c0030c09dc45
Kambiz Aghaiepour <kambiz@redhat.com>
2017-07-12 11:36:13 -0400

    Fix the box.
    Under certain circumstances it erroneously gets stuck
    and spams your IRC channel depending on if the patchset
    is not yet merged and there are multiple pending reviews.

--- misc-tools/irc-git-review-announce.sh
+++ misc-tools/irc-git-review-announce.sh
@@ -34,13 +34,20 @@ for review in $(awk '{ print $1 }' $LISTING) ; do
   info="$(egrep ^$review $LISTING)"
   id=$(echo "$info" | awk '{ print $1 }')
   branch=$(echo "$info" | awk '{ print $2 }')
-  if [ ! -f $LISTING_STATEDIR/$review ]; then
-    msg="$(echo "$info" | sed 's/[0-9][0-9]*[ ][ ]*[^ ][^ ]*[ ][ ]*\(.*\)/\1/g')"
-    echo "#quads New Review  :: https://review.gerrithub.io/#/c/$id/  || branch: $branch || $msg" | nc 127.0.0.1 5556
+
+  git checkout master
+
+  if [ "$(git log --pretty=oneline | grep "$latestline")" ]; then
+    :
   else
-    if [ x"$(echo "$latestline" | awk '{ print $1 }')" != x"$(head -1 $LISTING_STATEDIR/$review | awk '{ print $1 }')" ]; then
-      msg="$(echo "$latestline" | sed 's/[^ ][^ ]*[ ][ ]*\(.*\)/\1/g')"
-      echo "#quads Review Updated :: https://review.gerrithub.io/#/c/$id/  || branch: $branch || $msg" | nc 127.0.0.1 5556
+    if [ ! -f $LISTING_STATEDIR/$review ]; then
+      msg="$(echo "$info" | sed 's/[0-9][0-9]*[ ][ ]*[^ ][^ ]*[ ][ ]*\(.*\)/\1/g')"
+      echo "#quads New Review  :: https://review.gerrithub.io/#/c/$id/  || branch: $branch || $msg" | nc 127.0.0.1 5556
+    else
+      if [ x"$(echo "$latestline" | awk '{ print $1 }')" != x"$(head -1 $LISTING_STATEDIR/$review | awk '{ print $1 }')" ]; then
+        msg="$(echo "$latestline" | sed 's/[^ ][^ ]*[ ][ ]*\(.*\)/\1/g')"
+        echo "#quads Review Updated :: https://review.gerrithub.io/#/c/$id/  || branch: $branch || $msg" | nc 127.0.0.1 5556
+      fi
     fi
   fi
   echo "$latestline" > $LISTING_STATEDIR/$review
